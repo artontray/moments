534945a5cff59a91aecc3816ac87f69db8caf6b1
Damien <db.webapp@gmail.com>
2023-03-06 16:09:57 +0000

    gdf

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,5 +4,5 @@ axios.defaults.baseURL = "https://rest-framework-api.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
-export const axiosReq = axios.create();
-export const axiosRes = axios.create();
+/*export const axiosReq = axios.create();
+export const axiosRes = axios.create();*/
