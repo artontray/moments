98a0084fa9c789dad6be5475b23c5968b90af554
Damien <db.webapp@gmail.com>
2023-03-07 15:13:07 +0000

    profile page done

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -16,7 +16,7 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Asset from "../../components/Asset";
 import { fetchMoreData } from "../../utils/utils";
-import PopularProfiles from "../profiles/PopularProfiles";
+
 
 function PostPage() {
   const { id } = useParams();
